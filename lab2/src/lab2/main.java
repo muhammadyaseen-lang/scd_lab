a4a61aec11e69d79f59bb2ce1af41939fcd9ecc5
muhammadyaseen-lang <muhammad.yaseen@riphah.edu.pk>
2025-10-14 00:29:07 +0500

    Update main.java

--- lab2/src/lab2/main.java
+++ lab2/src/lab2/main.java
@@ -8,11 +8,11 @@ public class main {
 		
 		int x=10;
 		int y=20;
-<<<<<<< HEAD
+
 		int z=20;
-=======
+
 int yaseen=100;
->>>>>>> 4537841f93285aad61c7ca22b65975abc48232ca
+
 		for(int i = 1;i<=10 ;i++)
 	    {
 		    for(int j = 1;j<=x ;j++)
